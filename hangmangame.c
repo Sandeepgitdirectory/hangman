706aaca3a99284f58ff9227ae5d95b993d866273
Sandeepgitdirectory <143493321+Sandeepgitdirectory@users.noreply.github.com>
2023-12-24 11:11:12 +0530

    Add files via upload

---
+++ hangmangame.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(){
+    int number,guess,nguesses;
+    srand(time(0));
+    number=rand()%100+1;//this funcn generates the number
+    do{
+        printf("Guess the number 1 and 100\n");
+        scanf("%d",&guess);
+        if(guess>number){
+            printf("Lower number please!\n");
+        }
+        else if(guess<number){
+            printf("Higher number please!\n");
+        }
+        else{
+            printf("You guessed the right number\n");
+        }
+        nguesses++;
+    }while(guess!=number);
+    
+    return 0;
+}
